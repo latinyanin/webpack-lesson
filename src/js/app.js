e089dd6471985fd03227408fffce3660030c3344
TaRo <latinyanin@gmail.com>
2020-12-17 08:16:49 +0200

    Start project

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+import '../../node_modules/focus-visible/dist/focus-visible';
+import $ from 'jquery';
+import 'popper.js';
+import '../scss/style.scss';
+import '../index.html';
+import 'bootstrap';
+import './main.js';
+
